13e2ad0e835ec62380c58556075b4fff388fd355
David <zurek.dav@gmail.com>
2024-02-04 17:57:33 +0100

    Add empty TrendingPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from "@angular/router"
+import { TrendingPageComponent } from "./pages/trending-page/trending-page.component"
 
-export const routes: Routes = []
+export const routes: Routes = [
+	{
+		path: "",
+		component: TrendingPageComponent
+	}
+]
---
+++ src/app/pages/trending-page/trending-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core"
+
+@Component({
+	standalone: true,
+	templateUrl: "./trending-page.component.html",
+	styleUrl: "./trending-page.component.scss"
+})
+export class TrendingPageComponent {}
