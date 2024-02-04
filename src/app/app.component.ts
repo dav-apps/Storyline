3e50f1d2eab88ddebc090f7c9b94e58ade953002
David <zurek.dav@gmail.com>
2024-02-04 17:37:20 +0100

    Install & setup dav-ui-components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from "@angular/core"
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
+import * as DavUIComponents from "dav-ui-components"
 
 @Component({
 	selector: "app-root",
 	standalone: true,
 	imports: [RouterOutlet],
 	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.scss"
+	styleUrl: "./app.component.scss",
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppComponent {}
+export class AppComponent {
+	constructor() {
+		DavUIComponents.setLocale("en-EN")
+	}
+}
