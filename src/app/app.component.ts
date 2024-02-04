6ba9ffd0f9258b7f0d9340be9e60ffcc85083cdc
David <zurek.dav@gmail.com>
2024-02-04 23:20:09 +0100

    Fix a bug with routing in the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core"
-import { RouterOutlet } from "@angular/router"
+import { RouterOutlet, RouterLink } from "@angular/router"
 import * as DavUIComponents from "dav-ui-components"
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, RouterLink],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 	schemas: [CUSTOM_ELEMENTS_SCHEMA]
