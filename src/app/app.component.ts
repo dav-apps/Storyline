c9c7e06409b909f0d5bd6833edef9be807a55754
David <zurek.dav@gmail.com>
2024-02-04 17:32:21 +0100

    Remove default content & format files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss"
 })
-export class AppComponent {
-  title = 'storyline';
-}
+export class AppComponent {}
