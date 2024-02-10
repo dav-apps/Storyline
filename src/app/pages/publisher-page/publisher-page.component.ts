d3ec9fb8eb1a770ac443057376d24a8ee9d7c87b
David <zurek.dav@gmail.com>
2024-02-11 00:09:22 +0100

    Implement infinite scroll on publisher page

--- src/app/pages/publisher-page/publisher-page.component.ts
+++ src/app/pages/publisher-page/publisher-page.component.ts
@@ -5,6 +5,7 @@ import {
 	faPlus
 } from "@fortawesome/pro-regular-svg-icons"
 import { ApiService } from "../../services/api-service"
+import { DataService } from "../../services/data-service"
 import { ArticleResource, PublisherResource } from "../../types"
 
 @Component({
@@ -13,24 +14,70 @@ import { ArticleResource, PublisherResource } from "../../types"
 })
 export class PublisherPageComponent {
 	publisher: PublisherResource = null
+	articles: ArticleResource[] = []
 	faArrowUpRightFromSquare = faArrowUpRightFromSquare
 	faPlus = faPlus
+	limit: number = 10
+	offset: number = 0
+	articlesLoading: boolean = false
 
 	constructor(
 		private apiService: ApiService,
+		private dataService: DataService,
 		private router: Router,
 		private activatedRoute: ActivatedRoute
 	) {}
 
 	async ngOnInit() {
 		const uuid = this.activatedRoute.snapshot.paramMap.get("uuid")
+		const publisher = await this.loadPublisher(uuid, this.limit, this.offset)
 
+		if (publisher == null) {
+			this.router.navigate([""])
+			return
+		}
+
+		this.publisher = publisher
+
+		for (let article of publisher.articles.items) {
+			this.articles.push(article)
+		}
+
+		this.dataService.contentContainer.addEventListener(
+			"scroll",
+			this.onScroll
+		)
+	}
+
+	ngOnDestroy() {
+		this.dataService.contentContainer.removeEventListener(
+			"scroll",
+			this.onScroll
+		)
+	}
+
+	onScroll = () => {
+		const contentContainer = this.dataService.contentContainer
+		const hasReachedBottom =
+			Math.abs(
+				contentContainer.scrollHeight -
+					contentContainer.scrollTop -
+					contentContainer.clientHeight
+			) < 1
+
+		if (hasReachedBottom) {
+			this.loadMoreArticles()
+		}
+	}
+
+	async loadPublisher(uuid: string, limit: number, offset: number) {
 		const response = await this.apiService.retrievePublisher(
 			`
+				uuid
 				name
 				url
 				logoUrl
-				articles {
+				articles(limit: $limit, offset: $offset) {
 					items {
 						uuid
 						title
@@ -38,13 +85,31 @@ export class PublisherPageComponent {
 					}
 				}
 			`,
-			{ uuid }
+			{ uuid, limit, offset }
 		)
 
 		const responseData = response.data?.retrievePublisher
-		if (responseData == null) return
+		if (responseData == null) return null
 
-		this.publisher = responseData
+		return responseData
+	}
+
+	async loadMoreArticles() {
+		if (this.articlesLoading) return
+		this.articlesLoading = true
+		this.offset += this.limit
+
+		const publisher = await this.loadPublisher(
+			this.publisher.uuid,
+			this.limit,
+			this.offset
+		)
+
+		for (let article of publisher.articles.items) {
+			this.articles.push(article)
+		}
+
+		this.articlesLoading = false
 	}
 
 	openWebsite() {
