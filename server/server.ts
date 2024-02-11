b5c50fec2321be81ac2c1552de425a908f5d25df
David <zurek.dav@gmail.com>
2024-02-11 16:28:34 +0100

    Add basic server project & integrate server in package scripts

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import * as path from "path"
+import express, { Request, Response } from "express"
+import { createServer } from "http"
+
+const app = express()
+const http = createServer(app)
+
+function getRoot(req: Request, res: Response) {
+	res.sendFile("./storyline/browser/index.html")
+}
+
+app.use(express.static("./storyline/browser"))
+
+app.get("/", getRoot)
+app.get("/*", getRoot)
+
+http.listen(3004)
