3b53dc9553ca66eee9898725358a0085526aa766
David <zurek.dav@gmail.com>
2024-02-14 23:26:53 +0100

    Show loading screen on article page during load

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core"
 import { Router, ActivatedRoute } from "@angular/router"
 import { faShareFromSquare } from "@fortawesome/pro-regular-svg-icons"
 import { ApiService } from "../../services/api-service"
+import { DataService } from "../../services/data-service"
 import { LocalizationService } from "../../services/localization-service"
 import { ArticleResource } from "../../types"
 
@@ -18,11 +19,13 @@ export class ArticlePageComponent {
 
 	constructor(
 		private apiService: ApiService,
+		private dataService: DataService,
 		private localizationService: LocalizationService,
 		private activatedRoute: ActivatedRoute,
 		private router: Router
 	) {
 		this.showShareButton = navigator.share != null
+		this.dataService.loadingScreenVisible = true
 	}
 
 	async ngOnInit() {
@@ -46,7 +49,12 @@ export class ArticlePageComponent {
 		)
 
 		const responseData = response.data?.retrieveArticle
-		if (responseData == null) return
+		this.dataService.loadingScreenVisible = false
+
+		if (responseData == null) {
+			this.router.navigate(["/"])
+			return
+		}
 
 		this.article = responseData
 	}
